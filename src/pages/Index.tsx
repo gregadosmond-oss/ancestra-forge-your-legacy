77e4fe83ea5c662e267b038c2fcb95a75a8c6a78
Gregory Osmond <gregadosmond@gmail.com>
2026-04-14 16:44:27 -0500

    fix: use CrestHero on landing page (same flat plane + chroma-key as Stop 4)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import LandingCrestHero from "@/components/LandingCrestHero";
+import CrestHero from "@/components/CrestHero";
 
 const Index = () => {
   return (
@@ -24,7 +24,7 @@ const Index = () => {
 
       {/* 3D Crest Hero */}
       <div className="relative z-10 w-full max-w-4xl">
-        <LandingCrestHero />
+        <CrestHero />
       </div>
 
       {/* Hero content */}
